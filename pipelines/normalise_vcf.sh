032780bf81bf6f0ac6febf0d67a51387bcbc30cc
Kai <zhentian.kai@outlook.com>
2020-08-20 16:39:08 +0800

    remove SV variants first

--- pipelines/normalise_vcf.sh
+++ pipelines/normalise_vcf.sh
@@ -13,11 +13,11 @@
 # ------------------------------------------------------------------------------------- #
 
 # --------------------------------- set parameters ------------------------------------ #
-bcftools="/usr/local/bin/bcftools"
-bgzip="/usr/local/bin/bgzip"
-tabix="/usr/local/bin/tabix"
-ref="/data/ngs/database/soft_database/GATK_Resource_Bundle/hg19/ucsc.hg19.fasta"
-target_bed="/data/ngs/database/bed/DF.bed"
+bcftools="/public/software/bcftools-1.9/bcftools"
+bgzip="/public/software/htslib-1.9/bin/bgzip"
+tabix="/public/software/htslib-1.9/bin/tabix"
+ref="/public/database/GATK_Resource_Bundle/hg19/ucsc.hg19.fasta"
+target_bed="/public/home/kai/各类标准品/WES/Exome_RefSeq_merged_probes_hg19.bed"
 minimal_depth="300"
 # ------------------------------------------------------------------------------------- #
 
@@ -36,28 +36,31 @@ for ifile in $input_folder/*vcf;
 do 
     sampleID=`basename ${ifile%%"."*}`;
 
-    # step1 - split multiallelic sites + left-alignment
+    # step1 - remove BND variant
+    grep -v "SVTYPE=BND" $ifile > ${output_folder}/${sampleID}.step1_snv.vcf;
+
+    # step2 - split multiallelic sites + left-alignment
     $bcftools norm -m -both -f ${ref} \
-    ${ifile} \
-    -o ${output_folder}/${sampleID}.step1_norm.vcf;
+    ${output_folder}/${sampleID}.step1_snv.vcf \
+    -o ${output_folder}/${sampleID}.step2_norm.vcf; 
 
-    # step2 - bgzip and make index file
-    $bgzip ${output_folder}/${sampleID}.step1_norm.vcf;
-    $tabix -p vcf ${output_folder}/${sampleID}.step1_norm.vcf.gz;
+    # step3 - bgzip and make index file
+    $bgzip ${output_folder}/${sampleID}.step2_norm.vcf;
+    $tabix -p vcf ${output_folder}/${sampleID}.step2_norm.vcf.gz;
 
-    # step3 - filter off-target variants
+    # step4 - filter off-target variants
     $bcftools view -R $target_bed \
-    ${output_folder}/${sampleID}.step1_norm.vcf.gz \
-    > ${output_folder}/${sampleID}.step2_on_target.vcf;
+    ${output_folder}/${sampleID}.step2_norm.vcf.gz \
+    > ${output_folder}/${sampleID}.step3_on_target.vcf;
 
-    # step4 - filter low-support variants
-    grep "#\|PASS" ${output_folder}/${sampleID}.step2_on_target.vcf > \
-    ${output_folder}/${sampleID}.step3_filter.vcf
+    # step5 - filter low-support variants
+    grep "#\|PASS" ${output_folder}/${sampleID}.step3_on_target.vcf > \
+    ${output_folder}/${sampleID}.step4_filter.vcf
 
     $bcftools filter -i "(FORMAT/AD[0:0]+AD[0:1]) >= ${minimal_depth}" \
-    ${output_folder}/${sampleID}.step3_filter.vcf > \
-    ${output_folder}/${sampleID}.step4.vcf;
-done
+    ${output_folder}/${sampleID}.step4_filter.vcf > \
+    ${output_folder}/${sampleID}.step5_filter.vcf;
+done 
 
 
 
